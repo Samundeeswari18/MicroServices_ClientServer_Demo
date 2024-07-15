692be03cbec43a1ba5d5df1079ebe46578386e9a
Samundeeswari Velusamy <sam.velusamy36@gmail.com>
2024-07-15 16:08:17 -0400

    EurekaClient-Two -School (MicroServices)

---
+++ src/main/java/com/sam/schoolservice_eurekaclient/SchoolServiceEurekaClientApplication.java
@@ -0,0 +1,16 @@
+package com.sam.schoolservice_eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+
+public class SchoolServiceEurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolServiceEurekaClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sam/schoolservice_eurekaclient/SchoolServiceEurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.schoolservice_eurekaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolServiceEurekaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
