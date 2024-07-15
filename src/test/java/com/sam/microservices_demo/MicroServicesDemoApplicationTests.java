c597be4de88809364aae5ad7f5bde88484228ae2
Samundeeswari Velusamy <sam.velusamy36@gmail.com>
2024-07-15 16:00:51 -0400

    Eureka Server (MicroServices)

---
+++ src/test/java/com/sam/microservices_demo/MicroServicesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.microservices_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServicesDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
